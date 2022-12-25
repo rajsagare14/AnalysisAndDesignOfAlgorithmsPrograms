da37f2f1ea0b2ca7207bd8f3dc90ee8a8662bb2f
unknown <rajsagare14@gmail.com>
2022-12-25 20:06:41 +0530

    Incomplete and Incorrect Code

--- merge.py
+++ merge.py
@@ -1,6 +1,25 @@
-def mergeSort():
-	pass
-def printList():
-	pass
+def mergeSort(array:list):
+	if(len(array)>1):
+		mid = len(array)//2
+		leftSubArray = array[:mid]
+		rightSubArray = array[mid:]
+		sortedArray = array
+		mergeSort(leftSubArray)
+		mergeSort(rightSubArray)
+		i=j=k=0
+		while(i<len(leftSubArray) and j<len(rightSubArray)):
+			if(leftSubArray[i] < rightSubArray[j]):
+				sortedArray.insert(leftSubArray[i],k)
+				i = i+1
+			else:
+				sortedArray.insert(rightSubArray[j],k)
+				j = j+1
+			k = k+1
+			printList(sortedArray)
+def printList(array:list):
+	for i in range(len(array)):
+		print(f"{array[i]} ")
+	print()
 if(__name__ == '__main__'):
-	pass
+	array1 = [55,44,66,33,77,22,88,11,99,1]
+	mergeSort(array1)
