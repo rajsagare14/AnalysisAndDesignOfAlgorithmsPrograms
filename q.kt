d21ce9700522a8212109fd7c80e0d4994f96f925
unknown <rajsagare14@gmail.com>
2022-12-28 10:12:45 +0530

    Merge Sort Completed

--- q.kt
+++
@@ -1,8 +0,0 @@
-val rg29 = findViewById<RadioGroup>(R.id.rg29)
-        rg29.setOnCheckedChangeListener(
-            RadioGroup.OnCheckedChangeListener { group, checkedId ->
-                val radio: RadioButton = findViewById(checkedId)
-                Toast.makeText(applicationContext," On checked change :"+
-                        " ${radio.text}",
-                    Toast.LENGTH_SHORT).show()
-            })
